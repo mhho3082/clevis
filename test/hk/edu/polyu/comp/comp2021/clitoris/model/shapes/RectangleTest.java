6522afd92bd63c8d92cd17610cd33cec559b49ab
Joseph <joseph.mok@connect.polyu.hk>
2021-10-03 23:31:08 +0800

    Unit test
    Added move test for Line and Rectangle
    
    Added a IsContains test for the point have minimum distance from outline of the shape is smaller than 0.05 [REQ11]

--- test/hk/edu/polyu/comp/comp2021/clitoris/model/shapes/RectangleTest.java
+++ test/hk/edu/polyu/comp/comp2021/clitoris/model/shapes/RectangleTest.java
@@ -16,14 +16,18 @@ import static org.junit.Assert.assertTrue;
  *
  * The test covers the Rectangle object created with integer and double.
  *
- * The IsContains1 check for 3 different cases:
+ * The IsContains1 check for 4 different cases:
  * 1. The point inside the Rectangle
  * 2. The point touch the line of the Rectangle
  * 3. The point outside the Rectangle
+ * 4. The point have minimum distance from outline of the shape is smaller than 0.05 [REQ11]
  *
  * The testList and testListShort check the class return correct value.
  *
  * The DuplicateShapeNameException and SizeIsZeroException check for the throw exception when creating the object.
+ *
+ * The moveTest check the value xLeft, xRight, yTop and yBottom after move.
+ * The move test with positive and negative move for both integer and double
  *
  *
  * @author Mok Ka Kiu
@@ -80,6 +84,30 @@ public class RectangleTest {
     public void testIsContains6() {
         // Outside the rectangle
         assertFalse(r2.contains(2.7, 11.5));
+    }
+
+    @Test
+    public void testIsContain7() {
+        // The point have minimum distance from outline of the shape is smaller than 0.05 [REQ11] (outside)
+        assertTrue(r1.contains(-4.049, 0));
+    }
+
+    @Test
+    public void testIsContain8() {
+        // The point have minimum distance from outline of the shape is larger than or equal to 0.05 [REQ11] (outside)
+        assertFalse(r1.contains(-4.05, 0));
+    }
+
+    @Test
+    public void testIsContain9() {
+        // The point have minimum distance from outline of the shape is smaller than 0.05 [REQ11] (outside)
+        assertTrue(r2.contains(5.2, 5.66));
+    }
+
+    @Test
+    public void testIsContain10() {
+        // The point have minimum distance from outline of the shape is larger than or equal to 0.05 [REQ11] (outside)
+        assertFalse(r2.contains(5.2, 5.65));
     }
 
     @Test
@@ -146,4 +174,47 @@ public class RectangleTest {
         new Rectangle("test3", 2.1, 3.5, 0, 0);
     }
 
+    @Test
+    public void testMove1() {
+        // Move with positive value
+        r1.move(12,3.2344);
+
+        assertEquals(8, r1.getXLeft(), 0);
+        assertEquals(15, r1.getXRight(), 0);
+        assertEquals(0.2344, r1.getYTop(), 0);
+        assertEquals(4.2344, r1.getYBottom(), 0);
+    }
+
+    @Test
+    public void testMove2() {
+        // Move with negative value
+        r1.move(-6.2131,-23);
+
+        assertEquals(-10.2131, r1.getXLeft(), 0);
+        assertEquals(-3.2131, r1.getXRight(), 0);
+        assertEquals(-26, r1.getYTop(), 0);
+        assertEquals(-22, r1.getYBottom(), 0);
+    }
+
+    @Test
+    public void testMove3() {
+        // Move with positive value
+        r2.move(0,1.214);
+
+        assertEquals(2.7, r2.getXLeft(), 0);
+        assertEquals(13.3, r2.getXRight(), 0);
+        assertEquals(6.914, r2.getYTop(), 0);
+        assertEquals(12.614, r2.getYBottom(), 0);
+    }
+
+    @Test
+    public void testMove4() {
+        // Move with negative value
+        r2.move(-123.3,-21.4);
+
+        assertEquals(-120.6, r2.getXLeft(), 0);
+        assertEquals(-110, r2.getXRight(), 0);
+        assertEquals(-15.7, r2.getYTop(), 0);
+        assertEquals(-10.2, r2.getYBottom(), 0);
+    }
 }
